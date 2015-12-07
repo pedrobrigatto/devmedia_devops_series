885ebfe1c4b0cf321c21de0d6fc0ec327e363e7f
Pedro Brigatto <pedrobrigatto@gmail.com>
2015-12-07 09:41:14 -0200

    Adicionando testes unitarios para a identificacao de cores das faixas

--- devops/model/src/test/java/br/com/devmedia/articles/devops/model/MapaDeCoresTest.java
+++ devops/model/src/test/java/br/com/devmedia/articles/devops/model/MapaDeCoresTest.java
@@ -1,10 +1,37 @@
 package br.com.devmedia.articles.devops.model;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.devmedia.articles.devops.model.exceptions.CorDeBaseInvalida;
+import br.com.devmedia.articles.devops.model.exceptions.CorDePrecisaoInvalida;
+
 /**
+ * Avalia o comportamento da classe responsável por gerenciar o mapa de cores utilizado para 
+ * cálculos nominais de resistência elétrica.
  * 
  * @author pedrobrigatto
  */
 public class MapaDeCoresTest {
 	
-	// Ainda não implementado.
+	@Test(expected=CorDePrecisaoInvalida.class)
+	public void testLeituraDePrecisaoDesconhecida() throws CorDePrecisaoInvalida {
+		MapaDeCores.lerPrecisao("abc");
+	}
+	
+	@Test
+	public void testLeituraDePrecisaoValida() throws CorDePrecisaoInvalida {
+		assertTrue((Double) MapaDeCores.lerPrecisao(MapaDeCores.DOURADO) == 0.05);
+	}
+	
+	@Test(expected=CorDeBaseInvalida.class)
+	public void testLeituraDeBaseInvalida() throws CorDeBaseInvalida {
+		MapaDeCores.lerValorBase("abc");
+	}
+	
+	@Test
+	public void testLeituraDeBaseValida() throws CorDeBaseInvalida {
+		assertTrue((Double) MapaDeCores.lerValorBase(MapaDeCores.PRETO) == 0);
+	}
 }
